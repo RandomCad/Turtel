2486c745b63e364243e90cdee5f5545db46a0661
RandomCad <mmont4850@gmail.com>
2023-12-28 18:32:18 +0100

    Updated cheking made id prity :-)

--- .github/CheckUniteTestFiles.sh
+++ .github/CheckUniteTestFiles.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
+echo 'Checking ./UnitTest/TestData/blanck.test'
 echo "da35999de689c8ea2e2e3b88e57ed24e02b7eb444c3cb2b5aa99ad90280cc76e ./UnitTest/TestData/blanck.test" | sha256sum --check --status 
 if (($? != 0)); then
   exit 1
 fi
 
+echo 'Checking ./UnitTest/TestData/WhiteFollowedByJiber.test'
 echo "a09f3b877fe7323ff717e3894c7f87c365aedcdac602a1599ab80affe736e036  ./UnitTest/TestData/WhiteFollowedByJiber.test" | sha256sum --check --status 
 if (($? != 0)); then
   exit 1
 fi
 
+echo 'Checking ./UnitTest/TestData/./UnitTest/TestData/Walk.test'
 echo "1a90e2a773aa08df2123f6e8810bd9d9ada29dd017e132f2b62c74edcd21f10e  ./UnitTest/TestData/Walk.test" | sha256sum --check --status 
 if (($? != 0)); then
   exit 1
