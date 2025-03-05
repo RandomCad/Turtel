4479a18eb47209ca0804191e9937a08bfbc55e16
RandomCad <mmont4850@gmail.com>
2025-03-05 11:43:02 +0100

    Update CLANG and LLV to 19.1 on fedora

--- src/LLVMInterface.cpp
+++ src/LLVMInterface.cpp
@@ -13,7 +13,7 @@
 
 #include <llvm/ADT/SmallVector.h>
 #include <llvm/Support/VirtualFileSystem.h>
-#include <llvm/Support/Host.h>
+#include <llvm/TargetParser/Host.h>
 #include <llvm/Support/Program.h>
 #include <llvm/ADT/IntrusiveRefCntPtr.h>
 #include <clang/Driver/Job.h>
@@ -69,9 +69,7 @@ void LLVMInterface::CallLLVM(){
   for(int i = 0; i < sdlArgs.size(); ++i){
     std::cerr << sdlArgs[i] << std::endl;
     args.push_back(sdlArgs[i].c_str());
-  }//*/
-  //args.push_back("-l");
-	//args.push_back("curl");
+  }
 	args.push_back("-v");		// verbose
 	args.push_back("-Werror");		// warnings as errors
   args.push_back("-lm"); ///< add the math lib to the linking stage
