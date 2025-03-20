d6dbdd3203282eb0e30bf21a3150876e837946d0
RandomCad <mmont4850@gmail.com>
2025-03-20 08:12:04 +0100

    Add a Variable discoverer to construct a function scope Variable object

---
+++ src/VariableVisitor.h
@@ -0,0 +1,13 @@
+
+#include "SceneBaseVisitor.h"
+#include "SceneParser.h"
+#include "src/Variable.h"
+#include <ParserRuleContext.h>
+#include <unordered_map>
+#include <string>
+class VarVisitor : public SceneBaseVisitor{
+  std::unordered_map<std::string, Variable> ret;
+  std::any visitVariable(SceneParser::VariableContext *ctx) override;
+  public:
+  std::unordered_map<std::string, Variable> getVariableContext(antlr4::ParserRuleContext *ctx);
+};
